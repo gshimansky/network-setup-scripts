43a368d822e42beaa4cbc31bbef0013764670f62
Gregory Shimansky <gregory.shimansky@intel.com>
2018-03-02 14:13:52 -0600

    Added TC launch scripts for build and stability tests

---
+++ tc-launch-scripts/stability.sh
@@ -0,0 +1,25 @@
+export NFF_GO_IMAGE_PREFIX=tcstability
+export PATH=/opt/go/bin:"$PATH"
+export GOROOT=/opt/go
+export GOPATH=%teamcity.agent.work.dir%
+export no_proxy='antsatel01,antsatel04.an.intel.com,antsatel05.an.intel.com,antsatel06.an.intel.com,github.intel.com'
+export http_proxy=http://fmproxyslb.ice.intel.com:911
+export ftp_proxy=http://fmproxyslb.ice.intel.com:911
+export https_proxy=http://fmproxyslb.ice.intel.com:911
+export HTTP_PROXY=$http_proxy
+export HTTPS_PROXY=$https_proxy
+sp0=2219
+sp1=2221
+sp2=2202
+echo MACHINE 0
+ssh vagrant@localhost -p $sp0 -o LogLevel=FATAL -o Compression=yes -o DSAAuthentication=yes -o IdentitiesOnly=yes -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o IdentityFile="/localdisk/tc_agent/vagrant/.vagrant/machines/tctestvms-0/virtualbox/private_key" sudo -E ./network-setup-scripts/reconfigure.sh ./network-setup-scripts/vm/dpdk/0809.sh
+echo MACHINE 1
+ssh vagrant@localhost -p $sp1 -o LogLevel=FATAL -o Compression=yes -o DSAAuthentication=yes -o IdentitiesOnly=yes -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o IdentityFile="/localdisk/tc_agent/vagrant/.vagrant/machines/tctestvms-1/virtualbox/private_key" sudo -E ./network-setup-scripts/reconfigure.sh ./network-setup-scripts/vm/dpdk/0809.sh
+echo MACHINE 2
+ssh vagrant@localhost -p $sp2 -o LogLevel=FATAL -o Compression=yes -o DSAAuthentication=yes -o IdentitiesOnly=yes -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o IdentityFile="/localdisk/tc_agent/vagrant/.vagrant/machines/tctestvms-2/virtualbox/private_key" sudo -E ./network-setup-scripts/reconfigure.sh ./network-setup-scripts/vm/dpdk/0809.sh
+dp0=2218
+dp1=2220
+dp2=2201
+export NFF_GO_HOSTS=localhost:$dp0,localhost:$dp1,localhost:$dp2
+cd test/framework/main
+./tf -config stability.json -hosts $NFF_GO_HOSTS
